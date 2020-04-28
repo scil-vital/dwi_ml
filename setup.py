7fac5ab415d7e0e4b3ed148b12ee0686ed6be3a0
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2020-04-28 13:00:00 -0400

    ENH: Refactor `setup.py` to host package information separately
    Refactor `setup.py` to host package information in a separate ` info.py` file.
    
    Take advantage of the commit to complete the informations about the package.

--- setup.py
+++ setup.py
@@ -1,95 +1,48 @@
-
+#!/usr/bin/env python
+"""Installation script for the DWI_ML package."""
 
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+from os.path import join as pjoin
 import glob
 
-here = path.abspath(path.dirname(__file__))
+from setup_helpers import read_vars_from
 
-# Get the long description from the relevant file
-with open(path.join(here, 'index.rst'), encoding='utf-8') as f:
+
+# Read package information
+info = read_vars_from(pjoin('dwi_ml', 'info.py'))
+
+
+this_directory = path.abspath(path.dirname(__file__))
+
+dwi_ml_readme_path = pjoin(this_directory, 'README.rst')
+with open(dwi_ml_readme_path, encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='dwi-ml',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',
-
-    description='ML functions for dwi user in Sherbrooke (SCIL, VITAL labs)',
+    name=info.NAME,
+    version=info.VERSION,
+    description=info.DESCRIPTION,
     long_description=long_description,
-
-    # The project's main homepage.
-    url='https://github.com/scil-vital/dwi-ml',
-
-    # Author details
-    author='scil-vital',
-    author_email='',
-
-    # Choose your license
-    license='None ATM',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'None ATM',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-    ],
-
-    # What does your project relate to?
-    keywords={'dwi-ml','ML', 'DWI', 'tractography'},
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
+    long_description_content_type='text/x-rst',
+    url=info.URL,
+    project_urls={'Bug tracker': info.BUG_TRACKER,
+                  'Documentation': info.DOCUMENTATION,
+                  'Source code': info.SOURCE_CODE},
+    license=info.LICENSE,
+    author=info.AUTHOR,
+    author_email=info.AUTHOR_EMAIL,
+    classifiers=info.CLASSIFIERS,
+    keywords=info.KEYWORDS,
+    maintainer=info.MAINTAINER,
+    provides=info.PROVIDES,
     packages=find_packages(exclude=['contrib', 'doc', 'unit_tests']),
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ["nibabel", "hdf5"],
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
+    requires=info.REQUIRES,
     package_data={},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     data_files=[],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # Note Emma: We could eventually use entry points. See discussion here:
-    # https://stackoverflow.com/questions/18787036/difference-between-entry-points-console-scripts-and-scripts-in-setup-py
     entry_points={},
     scripts=glob.glob("scripts_python/*/*.py")
 )

37226740c2360905ba90c015355bf451f9daa6d2
Emmanuelle Renauld <emmanuelle.renauld@usherbrooke.ca>
2020-05-09 10:56:13 -0400

    Clarified variable names

--- dwi_ml/data/processing/streamlines/data_augmentation.py
+++ dwi_ml/data/processing/streamlines/data_augmentation.py
@@ -99,14 +99,14 @@ def cut_random_streamlines(
 
 
 # Checked!
-def reverse_streamlines(sft: StatefulTractogram, flip_ids: np.ndarray = None):
+def reverse_streamlines(sft: StatefulTractogram, reverse_ids: np.ndarray = None):
     """Reverse streamlines, i.e. inverse the beginning and end
 
     Parameters
     ----------
     sft: StatefulTractogram
         Dipy object containing your streamlines
-    flip_ids: np.ndarray, optional
+    reverse_ids: np.ndarray, optional
         List of streamlines to reverse. If not provided, all streamlines are
         reversed.
 
@@ -115,15 +115,15 @@ def reverse_streamlines(sft: StatefulTractogram, flip_ids: np.ndarray = None):
     new_sft: StatefulTractogram
         Dipy object with reversed streamlines and data_per_point.
     """
-    if flip_ids is None:
-        flip_ids = range(len(sft.streamlines))
+    if reverse_ids is None:
+        reverse_ids = range(len(sft.streamlines))
 
-    new_streamlines = [s[::-1] if i in flip_ids else s for i, s in
+    new_streamlines = [s[::-1] if i in reverse_ids else s for i, s in
                        enumerate(sft.streamlines)]
     new_data_per_point = copy.deepcopy(sft.data_per_point)
     for key in sft.data_per_point:
-        new_data_per_point[key] = [d[::-1] if i in flip_ids else d for i, d in
-                                   enumerate(new_data_per_point[key])]
+        new_data_per_point[key] = [d[::-1] if i in reverse_ids else d for i, d
+                                   in enumerate(new_data_per_point[key])]
 
     new_sft = StatefulTractogram.from_sft(
         new_streamlines, sft, data_per_point=new_data_per_point,

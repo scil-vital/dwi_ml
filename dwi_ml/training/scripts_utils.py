7c68df253370346f333c868379e718d95524ca6f
Emmanuelle Renauld <emmanuelle.renauld@usherbrooke.ca>
2020-06-09 08:36:50 -0400

    Checking instance type

--- dwi_ml/training/scripts_utils.py
+++ dwi_ml/training/scripts_utils.py
@@ -2,8 +2,9 @@
 
 from os import path
 
+### Various arg_parser equivalents.
 
-def check_none(var, var_name: str):
+def check_similar_to_none(var, var_name: str):
     """
     In yaml, None is understood with ~ or with 'null'. Here, we make sure
     that user didn't write something else close to null, which would be
@@ -32,11 +33,38 @@ def check_required_was_given(var, var_name: str, required: bool):
                          "None.".format(var_name))
 
 
-def check_bool_was_given(var: bool, var_name: str):
-    if var is None:
-        raise ValueError("You have set {} to None (in yaml, ~ or null). A bool "
-                         "was expected (true or false).".format(var_name))
+def check_bool_instance(var: bool, var_name: str):
+    # In case of bools, we do not allow the value None.
+    if not isinstance(var, bool):
+        raise ValueError('A bool value was expected for the variable {} but '
+                         '{} was received'.format(var_name, var))
 
+
+def check_float_instance(var: float, var_name: str):
+    # We allow the value None.
+    if var is not None and not isinstance(var, float):
+        if isinstance(var, int):
+            var = float(var)
+        else:
+            raise ValueError('A float value was expected for the variable {} but '
+                             '{} was received.'.format(var_name, var))
+    return var
+
+
+def check_int_instance(var: int, var_name: str):
+    # We allow the value None.
+    if var is not None and not isinstance(var, int):
+        raise ValueError("We expected a int for the variable {} but {} was "
+                         "received".format(var_name, var))
+
+
+def check_str_instance(var: str, var_name: str):
+    # We allow the value None.
+    if var is not None and not isinstance(var, str):
+        raise ValueError('The variable {} should be a string, but {} was '
+                         'received'.format(var_name, var))
+
+### Checks for each parameters
 
 
 def check_logging_level(level: str, required: bool = False):
@@ -44,8 +72,9 @@ def check_logging_level(level: str, required: bool = False):
     Checks that level is one of ['error', 'warning', 'info', 'debug'] and
     returns level in upper case. If None and not required, default is warning.
     """
-    check_none(level, 'logging level')
+    check_similar_to_none(level, 'logging level')
     check_required_was_given(level, 'logging level', required)
+    check_str_instance(level, 'level')
 
     if level is None:
         level = 'WARNING'
@@ -59,13 +88,15 @@ def check_logging_level(level: str, required: bool = False):
 
 
 def check_experiment_name(name: str, required: bool = False):
-    check_none(name, 'experiment name')
+    check_similar_to_none(name, 'experiment name')
     check_required_was_given(name, 'experiment name', required)
+    check_str_instance(name, 'name')
+
     return name
 
 
 def check_hdf5_filename(filename: str, required: bool = False):
-    check_none(filename, 'hdf5 filename')
+    check_similar_to_none(filename, 'hdf5 filename')
     check_required_was_given(filename, 'hdf5 filename', required)
 
     if filename is not None and not path.exists(filename):
@@ -75,7 +106,7 @@ def check_hdf5_filename(filename: str, required: bool = False):
 
 
 def check_training_subjs_filename(filename: str, required: bool = False):
-    check_none(filename, 'training subjs filename')
+    check_similar_to_none(filename, 'training subjs filename')
     check_required_was_given(filename, 'training subjs filename', required)
 
     if filename is not None and not path.exists(filename):
@@ -85,7 +116,7 @@ def check_training_subjs_filename(filename: str, required: bool = False):
 
 
 def check_validation_subjs_filename(filename: str, required: bool = False):
-    check_none(filename, 'validation subjs filename')
+    check_similar_to_none(filename, 'validation subjs filename')
     check_required_was_given(filename, 'validation subjs filename', required)
 
     if filename is not None and not path.exists(filename):
@@ -95,29 +126,34 @@ def check_validation_subjs_filename(filename: str, required: bool = False):
 
 
 def check_step_size(step_size: float, required: bool = False):
-    check_none(step_size, 'step size')
+    check_similar_to_none(step_size, 'step size')
     check_required_was_given(step_size, 'step_size', required)
+    step_size = check_float_instance(step_size, 'step_size')
 
     if step_size is not None and step_size <= 0:
         raise ValueError("Step size can't be 0 or less!")
-        # Note. When using scilpy.tracking.tools.resample_streamlines_step_size,
-        # a warning is shown if step_size < 0.1 or > np.max(sft.voxel_sizes),
-        # saying that the value is suspicious. Not raising the same warnings
-        # here as you may be wanting to test weird things to understand better
+        # Note. When using
+        # scilpy.tracking.tools.resample_streamlines_step_size, a warning
+        # is shown if step_size < 0.1 or > np.max(sft.voxel_sizes), saying
+        # that the value is suspicious. Not raising the same warnings here
+        # as you may be wanting to test weird things to understand better
         # your model.
     return step_size
 
 
 def check_add_noise(add_streamline_noise: bool):
-    check_bool_was_given(add_streamline_noise, 'add_streamline_noise')
+    check_bool_instance(add_streamline_noise, 'add_streamline_noise')
+
     return add_streamline_noise
 
 
 def check_split_ratio(split_ratio: float):
-    check_none(split_ratio, 'split_ratio')
+    # Not required. If not given, value is 0.
+    check_similar_to_none(split_ratio, 'split_ratio')
+    check_float_instance(split_ratio, 'split_ratio')
 
     if split_ratio is None:
-        split_ratio = 0
+        split_ratio = 0.0
     elif split_ratio > 1 or split_ratio < 0:
         raise ValueError("split_ratio must be a ratio (i.e. between 0 and 1).")
     return split_ratio
@@ -125,10 +161,10 @@ def check_split_ratio(split_ratio: float):
 
 def check_neighborhood(sphere_radius: float, grid_radius: int):
     """
-    Returns: a tuple (type = 'sphere' or 'grid', radius)
+    Returns: a tuple (str, [float | int]) = ('sphere' or 'grid', radius)
     """
-    check_none(sphere_radius, 'neighborhood sphere radius')
-    check_none(grid_radius, 'neighborhood grid radius')
+    check_similar_to_none(sphere_radius, 'neighborhood sphere radius')
+    check_similar_to_none(grid_radius, 'neighborhood grid radius')
 
     if sphere_radius and grid_radius:
         raise ValueError("You must choose maximum one neighborhood option "
@@ -143,7 +179,8 @@ def check_neighborhood(sphere_radius: float, grid_radius: int):
 
 def check_previous_dir(add_x_previous_dirs: int):
     # Not required. If not given, value is 0.
-    check_none(add_x_previous_dirs, 'add_x_previous_dirs')
+    check_similar_to_none(add_x_previous_dirs, 'add_x_previous_dirs')
+    check_int_instance(add_x_previous_dirs, 'add_x_previous_dirs')
 
     if add_x_previous_dirs is None:
         add_x_previous_dirs = 0
@@ -151,32 +188,41 @@ def check_previous_dir(add_x_previous_dirs: int):
 
 
 def check_max_epochs(max_epochs: int, required: bool = False):
-    check_none(max_epochs, 'max_epochs')
+    check_similar_to_none(max_epochs, 'max_epochs')
     check_required_was_given(max_epochs, 'max_epochs', required)
+    check_int_instance(max_epochs, 'max_epochs')
+
     return max_epochs
 
 
 def check_patience(patience: int, required: bool = False):
-    check_none(patience, 'patience')
+    check_similar_to_none(patience, 'patience')
     check_required_was_given(patience, 'patience', required)
+    check_int_instance(patience, 'patience')
+
     return patience
 
 
 def check_batch_size(batch_size: int, required: bool = False):
-    check_none(batch_size, 'batch_size')
+    check_similar_to_none(batch_size, 'batch_size')
     check_required_was_given(batch_size, 'batch_size', required)
+    check_int_instance(batch_size, 'batch_size')
+
     return batch_size
 
 
 def check_volumes_per_batch(volumes_per_batch: int, required: bool = False):
-    check_none(volumes_per_batch, 'volumes_per_batch')
+    check_similar_to_none(volumes_per_batch, 'volumes_per_batch')
     check_required_was_given(volumes_per_batch, 'volumes_per_batch', required)
+    check_int_instance(volumes_per_batch, 'volumes_per_batch')
+
     return volumes_per_batch
 
 
 def check_cycles_per_volume(cycles_per_volume: int, volumes_per_batch: int):
     # Not required (if volumes_per_batch is None). If not given, value is 1.
-    check_none(cycles_per_volume, 'cycles_per_volume')
+    check_similar_to_none(cycles_per_volume, 'cycles_per_volume')
+    check_int_instance(cycles_per_volume, 'cycles_per_volume')
 
     if cycles_per_volume is not None and volumes_per_batch is None:
         raise ValueError("You provided a value for cycles_per_volume_batch "
@@ -189,12 +235,13 @@ def check_cycles_per_volume(cycles_per_volume: int, volumes_per_batch: int):
 
 
 def check_lazy(lazy: bool):
-    check_bool_was_given(lazy, 'lazy')
+    check_bool_instance(lazy, 'lazy')
+
     return lazy
 
 
 def check_cache_manager(cache_manager: bool, lazy: bool):
-    check_bool_was_given(cache_manager, 'cache_manager')
+    check_bool_instance(cache_manager, 'cache_manager')
 
     if cache_manager and not lazy:
         raise ValueError("You set cache_manager to True but lazy is not used. "
@@ -203,29 +250,34 @@ def check_cache_manager(cache_manager: bool, lazy: bool):
 
 
 def check_use_gpu(use_gpu: bool):
-    check_bool_was_given(use_gpu, 'use_gpu')
+    check_bool_instance(use_gpu, 'use_gpu')
 
     return use_gpu
 
 
 def check_num_cpu_workers(num_workers: int, required: bool = False):
-    check_none(num_workers, 'num_workers')
+    check_similar_to_none(num_workers, 'num_workers')
     check_required_was_given(num_workers, 'num_workers', required)
+    check_int_instance(num_workers, 'num_workers')
+
     return num_workers
 
 
 def check_worker_interpolation(worker_interpolation: bool):
-    check_bool_was_given(worker_interpolation, 'worker_interpolation')
+    check_bool_instance(worker_interpolation, 'worker_interpolation')
 
     return worker_interpolation
 
 
 def check_taskman_managed(taskman_managed: bool):
-    check_bool_was_given(taskman_managed, 'taskman_managed')
+    check_bool_instance(taskman_managed, 'taskman_managed')
+
     return taskman_managed
 
 
 def check_seed(seed: int, required: bool = False):
-    check_none(seed, 'seed')
+    check_similar_to_none(seed, 'seed')
     check_required_was_given(seed, 'seed', required)
+    check_int_instance(seed, 'seed')
+
     return seed

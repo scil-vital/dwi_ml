ebf2c3667b343646668f25b59174a0cc724b303e
Emmanuelle Renauld <emmanuelle.renauld@usherbrooke.ca>
2021-06-07 12:54:19 -0400

    Adding option enfoce_bundles_presence in ALL_STEPS

--- please_copy_and_adapt/ALL_STEPS.sh
+++ please_copy_and_adapt/ALL_STEPS.sh
@@ -17,9 +17,11 @@ bundles="AF_left AF_right CC_1 CC_2 FX_left FX_right"
 # OR bundles=$(<bundles.txt) to read a list of bundles from a text file.
 mask="masks/wm_mask.nii.gz"
 space="rasmm"
+enforce_bundles_presence=True # True if you want the script to stop if a bundle is missing for some subject
 
 create_hdf5_dataset.py --name $name --bundles $bundles --std_mask $mask \
     --space $space --save_intermediate --logging debug \
+    --enforce_bundles_presence $enforce_bundles_presence
     $database_folder $config_file $training_subjs $validation_subjs
 
 ########################

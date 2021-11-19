44e565967dfab13adf259aea43002a9e91b894b1
Emmanuelle Renauld <emmanuelle.renauld@usherbrooke.ca>
2021-11-19 16:31:47 -0500

    Re-ran test_direction_getter. OK!

--- tests/running_tests.sh
+++ tests/running_tests.sh
@@ -1,8 +1,12 @@
 
+tests/test_direction_getter_models.py
+
 # Create any hdf5 dataset. Ex, use please_copy_and_adapt/ALL_STEPS.sh
 
 tests/test_multisubjectdataset_creation_from_hdf5.py $hdf5_filename
 tests/test_batch_sampler_iter.py $hdf5_filename
+
+# Note. This also tests neighborhoods in the model:
 tests/test_batch_sampler_load_batch.py $hdf5_filename $ref $test_tractograms_path
 
 # check results and then:

b02e65fa8ed7b1e4e60707d9e64235d8b9158260
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-02-27 12:53:08 -0500

    Add test

---
+++ scripts_python/tests/test_compute_loss_copy_previous.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+from dwi_ml.tests.utils.data_and_models_for_tests import fetch_testing_data
+
+data_dir = fetch_testing_data()
+
+
+def test_help_option(script_runner):
+    ret = script_runner.run('dwiml_compute_loss_copy_previous.py', '--help')
+    assert ret.success
+
+
+def test_running(script_runner):
+    sft = os.path.join(data_dir, 'dwi_ml_ready/subjX/example_bundle/Fornix.trk')
+    ret = script_runner.run('dwiml_compute_loss_copy_previous.py', sft,
+                            'sphere-classification', '--step_size', '0.5')
+    assert ret.success
+

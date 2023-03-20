f4572157c39c574cb3c5642c5dae7984410177c9
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-03-20 12:32:38 -0400

    Remove scilpy's super tracker

--- dwi_ml/tracking/tracker.py
+++ dwi_ml/tracking/tracker.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
+import itertools
 import logging
 import multiprocessing
 import os
+import sys
+import traceback
 from typing import List
 
 import numpy as np
 import torch
 from dipy.io.stateful_tractogram import Space, Origin
+from dipy.tracking.streamlinespeed import compress_streamlines
 from scilpy.image.datasets import DataVolume
-from scilpy.tracking.tracker import Tracker as ScilpyTracker
 from scilpy.tracking.seed import SeedGenerator
 
 from dwi_ml.tracking.propagator import DWIMLPropagator
@@ -16,11 +19,7 @@ from dwi_ml.tracking.propagator import DWIMLPropagator
 logger = logging.getLogger('tracker_logger')
 
 
-class DWIMLTracker(ScilpyTracker):
-    # Removing a few warning by explicitly telling python that the
-    # propagator type is not as in super. Supported in python > 3.5
-    propagator: DWIMLPropagator
-
+class DWIMLTracker:
     def __init__(self, propagator: DWIMLPropagator, mask: DataVolume,
                  seed_generator: SeedGenerator, nbr_seeds, min_nbr_pts,
                  max_nbr_pts, max_invalid_dirs, compression_th=0.1,
@@ -31,7 +30,7 @@ class DWIMLTracker(ScilpyTracker):
         Parameters: See scilpy.
         ----------
         propagator: DWIMLPropagator
-            Now a dwi_ml version of the propagator.
+            Your propagator.
         simultaneous_tracking: bool
             If true, track multiple lines at the same time. Intended for GPU.
         use_gpu: bool
@@ -39,15 +38,39 @@ class DWIMLTracker(ScilpyTracker):
         hdf_file:
             Necessary to open a new hdf handle a at process.
         """
+        self.propagator = propagator
+        self.mask = mask
+        self.seed_generator = seed_generator
+        self.nbr_seeds = nbr_seeds
+        self.min_nbr_pts = min_nbr_pts
+        self.max_nbr_pts = max_nbr_pts
+        self.max_invalid_dirs = max_invalid_dirs
+        self.compression_th = compression_th
+        self.save_seeds = save_seeds
+        self.mmap_mode = None
+        self.rng_seed = rng_seed
+        self.track_forward_only = track_forward_only
+        self.skip = 0  # Not supported yet.
+        self.printing_frequency = 1
 
-        # Warning about the mask being an MRIData instead of DataVolume but
-        # ok! Modified to be able to use as tensor more easily for torch.
-        mmap_mode = None  # Not used here, we deal with hdf5.
-        super().__init__(propagator, mask, seed_generator, nbr_seeds,
-                         min_nbr_pts, max_nbr_pts, max_invalid_dirs,
-                         compression_th, nbr_processes, save_seeds, mmap_mode,
-                         rng_seed, track_forward_only)
+        # Space and origin
+        self.origin = self.propagator.origin
+        self.space = self.propagator.space
+        assert self.space == Space.VOX
+        assert self.origin == Origin('corner')
+        if (seed_generator.origin != propagator.origin or
+                seed_generator.space != propagator.space):
+            raise ValueError("Seed generator and propagator must work with "
+                             "the same space and origin!")
 
+        # Nb points
+        if self.min_nbr_pts <= 0:
+            logging.warning("Minimum number of points cannot be 0. Changed to "
+                            "1.")
+            self.min_nbr_pts = 1
+
+        # Either GPU or multi-processes
+        self.nbr_processes = self._set_nbr_processes(nbr_processes)
         if nbr_processes > 2:
             if not propagator.dataset.is_lazy:
                 raise ValueError("Multiprocessing only works with lazy data.")
@@ -55,10 +78,8 @@ class DWIMLTracker(ScilpyTracker):
                 raise ValueError("You cannot use both multi-processes and "
                                  "gpu.")
 
-        # Set device
         self.simultanenous_tracking = simultanenous_tracking
         self.use_gpu = use_gpu
-
         if use_gpu:
             if torch.cuda.is_available():
                 self.device = torch.device('cuda')
@@ -68,55 +89,174 @@ class DWIMLTracker(ScilpyTracker):
                                  "available!")
         else:
             self.device = torch.device('cpu')
-
-        # Sending model and data to device.
-        self.propagator.move_to(self.device)
+        self.propagator.move_to(self.device)  # Sends model and data to device
 
         logger.setLevel(log_level)
 
-        # Increase the printing frequency as compared to super
-        self.printing_frequency = 1
+    def _set_nbr_processes(self, nbr_processes):
+        """
+        Copied from scilpy's tracker.
 
-        # NOTE: TRAINER USES STREAMLINES COORDINATES IN VOXEL SPACE, TO CORNER.
-        assert self.space == Space.VOX
-        assert self.origin == Origin('corner')
-        assert self.seed_generator.space == Space.VOX
-        assert self.seed_generator.origin == Origin('corner')
+        If user did not define the number of processes, define it automatically
+        (or set to 1 -- no multiprocessing -- if we can't).
+        """
+        if nbr_processes <= 0:
+            try:
+                nbr_processes = multiprocessing.cpu_count()
+            except NotImplementedError:
+                logging.warning("Cannot determine number of cpus: "
+                                "nbr_processes set to 1.")
+                nbr_processes = 1
+
+        if nbr_processes > self.nbr_seeds:
+            nbr_processes = self.nbr_seeds
+            logging.debug("Setting number of processes to {} since there were "
+                          "less seeds than processes.".format(nbr_processes))
+        return nbr_processes
 
     def track(self):
         """
         Scilpy's Tracker.track():
-            - calls _get_streamlines, or
-            - deals with parallel sub-processes and calls _get_streamlines_sub,
-              which call _get_streamlines for each process.
+            - calls _cpu_tracking, or
+            - deals with parallel sub-processes and calls _cpu_tracking_sub,
+              which call _cpu_tracking for each process.
 
         Here adding the GPU usage. Other changes for dwi_ml will be reflected
-        in _get_streamlines.
+        in _cpu_tracking.
         """
         if self.simultanenous_tracking > 1:
-            return self.simultanenous_tracking_on_gpu()
+            return self._gpu_simultanenous_tracking()
         else:
             # On CPU, with possibility of parallel processing.
-            return super().track()
+            # Copied from scilpy's tracker.
+            if self.nbr_processes < 2:
+                chunk_id = 1
+                lines, seeds = self._cpu_tracking(chunk_id)
+            else:
+                # Each process will use get_streamlines_at_seeds
+                chunk_ids = np.arange(self.nbr_processes)
 
-    def _prepare_multiprocessing_pool(self, tmpdir=None):
+                pool = self._cpu_prepare_multiprocessing_pool()
+
+                lines_per_process, seeds_per_process = zip(*pool.map(
+                    self._cpu_tracking_sub, chunk_ids))
+                pool.close()
+                # Make sure all worker processes have exited before leaving
+                # context manager.
+                pool.join()
+                lines = [line for line in itertools.chain(*lines_per_process)]
+                seeds = [seed for seed in itertools.chain(*seeds_per_process)]
+
+            return lines, seeds
+
+    def _cpu_prepare_multiprocessing_pool(self):
         """
         Prepare multiprocessing pool. We do not need to deal with data
         management, contrary to scilpy.
         """
         # Clearing data from memory
-        self.propagator.reset_data(reload_data=False)
+        self.propagator.reset_data()
 
         pool = multiprocessing.Pool(self.nbr_processes)
         return pool
 
-    def _reload_data_for_new_process(self, init_args):
+    def _cpu_reload_data_for_new_process(self):
         """Nothing to do here. hdf5 deals well with multi-processes."""
 
         logger.info("Preparing for process id {}".format(os.getpid()))
-        self.propagator.reset_data(reload_data=True)
+        self.propagator.reset_data()
 
-    def simultanenous_tracking_on_gpu(self):
+    def _cpu_tracking_sub(self, chunk_id):
+        """
+        multiprocessing.pool.map input function. Calls the main tracking
+        method (_cpu_tracking) with correct initialization arguments
+        (taken from the global variable multiprocess_init_args).
+
+        Parameters
+        ----------
+        chunk_id: int
+            This processes's id.
+
+        Return
+        -------
+        lines: list
+            List of list of 3D positions (streamlines).
+        """
+        self._cpu_reload_data_for_new_process()
+        try:
+            streamlines, seeds = self._cpu_tracking(chunk_id)
+            return streamlines, seeds
+        except Exception as e:
+            logging.error("Operation _cpu_tracking_sub() failed.")
+            traceback.print_exception(*sys.exc_info(), file=sys.stderr)
+            raise e
+
+    def _cpu_tracking(self, chunk_id):
+        """
+        Tracks the n streamlines associates with current process (identified by
+        chunk_id). The number n is the total number of seeds / the number of
+        processes. If asked by user, may compress the streamlines and save the
+        seeds.
+
+        Parameters
+        ----------
+        chunk_id: int
+            This process ID.
+
+        Returns
+        -------
+        streamlines: list
+            The successful streamlines.
+        seeds: list
+            The list of seeds for each streamline, if self.save_seeds. Else, an
+            empty list.
+        """
+        streamlines = []
+        seeds = []
+
+        # Initialize the random number generator to cover multiprocessing,
+        # skip, which voxel to seed and the subvoxel random position
+        chunk_size = int(self.nbr_seeds / self.nbr_processes)
+        first_seed_of_chunk = chunk_id * chunk_size + self.skip
+        random_generator, indices = self.seed_generator.init_generator(
+            self.rng_seed, first_seed_of_chunk)
+        if chunk_id == self.nbr_processes - 1:
+            chunk_size += self.nbr_seeds % self.nbr_processes
+
+        # Getting streamlines
+        for s in range(chunk_size):
+            if s % self.printing_frequency == 0:
+                logging.info("Process {} (id {}): {} / {}"
+                             .format(chunk_id, os.getpid(), s, chunk_size))
+
+            seed = self.seed_generator.get_next_pos(
+                random_generator, indices, first_seed_of_chunk + s)
+
+            # Forward and backward tracking
+            line = self._get_multiple_lines_both_directions([seed])[0]
+
+            if line is not None:
+                streamline = np.array(line, dtype='float32')
+
+                if self.compression_th and self.compression_th > 0:
+                    # Compressing. Threshold is in mm. Verifying space.
+                    if self.space == Space.VOX:
+                        # Equivalent of sft.to_voxmm:
+                        streamline *= self.seed_generator.voxres
+                        compress_streamlines(streamline, self.compression_th)
+                        # Equivalent of sft.to_vox:
+                        streamline /= self.seed_generator.voxres
+                    else:
+                        compress_streamlines(streamline, self.compression_th)
+
+                streamlines.append(streamline)
+
+                if self.save_seeds:
+                    seeds.append(np.asarray(seed, dtype='float32'))
+
+        return streamlines, seeds
+        
+    def _gpu_simultanenous_tracking(self):
         """
         Creating all seeds at once and propagating all streamlines together.
         """
@@ -149,15 +289,8 @@ class DWIMLTracker(ScilpyTracker):
 
         return lines, seeds
 
-    def _get_line_both_directions(self, seed: np.ndarray):
-        # Only using our torch-based version. Skipping scilpy's single-version.
-        self._get_multiple_lines_both_directions([seed])
-
     def _get_multiple_lines_both_directions(self, n_seeds: List[np.ndarray]):
         """
-        Equivalent of super's() _get_line_both_directions() for multiple lines
-        tracking at the same time (meant to be used on GPU).
-
         Params
         ------
         n_seeds : List[np.ndarray]
@@ -275,3 +408,21 @@ class DWIMLTracker(ScilpyTracker):
             final_lines, final_tracking_info, self.mask)
 
         return final_lines
+
+    def _verify_stopping_criteria(self, invalid_direction_count, last_pos):
+
+        # Checking number of consecutive invalid directions
+        if invalid_direction_count > self.max_invalid_dirs:
+            return False
+
+        # Checking if out of bound
+        if not self.mask.is_coordinate_in_bound(
+                *last_pos, space=self.space, origin=self.origin):
+            return False
+
+        # Checking if out of mask
+        if self.mask.get_value_at_coordinate(
+                *last_pos, space=self.space, origin=self.origin) <= 0:
+            return False
+
+        return True

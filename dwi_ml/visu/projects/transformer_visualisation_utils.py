dbbe895bfbbda6093932ff2391a8a97e06aa1460
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-03-22 15:29:40 -0400

    Add visu for ttst

--- dwi_ml/visu/projects/transformer_visualisation_utils.py
+++ dwi_ml/visu/projects/transformer_visualisation_utils.py
@@ -15,13 +15,12 @@ from scilpy.utils.streamlines import compress_sft
 
 from dwi_ml.experiment_utils.prints import add_logging_arg
 from dwi_ml.models.projects.transforming_tractography import \
-    AbstractTransformerModel, OriginalTransformerModel
+    AbstractTransformerModel
 from dwi_ml.tracking.utils import prepare_dataset_one_subj
 
 # Currently, with our quite long sequences compared to their example, this
 # is a bit ugly.
 SHOW_MODEL_VIEW = False
-NORMALIZE_WEIGHTS = True
 INFORMATIVE_TOKENS = False
 
 
@@ -153,49 +152,21 @@ def load_data_run_model(parser, args, model: AbstractTransformerModel,
         _, weights = model(
             batch_input, streamlines,
             return_weights=True, average_heads=args.average_heads)
-    return sft, weights
+    return weights, streamlines
 
 
-def tto_visualize_weights(args, parser):
-    sub_logger_level = args.logging.upper()
-    if sub_logger_level == 'DEBUG':
-        # make them info max
-        sub_logger_level = 'INFO'
-    logging.getLogger().setLevel(level=args.logging)
-
-    logging.info("Loading model.")
-    model = OriginalTransformerModel.load_params_and_state(
-        args.experiment_path + '/best_model', log_level=sub_logger_level)
-    model.set_context('visu')
-
-    sft, weights = load_data_run_model(parser, args, model)
-    sa_encoder, sa_decoder, mha_cross = weights
-
-    # ----------------
-    # Shape of attentions are a List of nb_layers x:
-    #                [nb_streamlines, (nb_heads), batch_max_len, batch_max_len]
-    # Reshaping all attentions to List of nb layers x
-    #                [nb_streamlines=1, nb_heads, this_s_len,   this_s_len]
-    # (nb heads dimension is skipped if args.average_heads).
-    # -----------------
-    nb_layers = len(sa_encoder)
-    nb_heads = 1 if args.average_heads else model.nheads
-
-    streamline1 = sft.streamlines[0]
-    this_seq_len = len(streamline1) - 1
-    logging.info("Chosen streamline's length: {} points"
-                 .format(len(streamline1)))
-
+def prepare_tokens(streamline):
+    this_seq_len = len(streamline) - 1  # nb directions = nb coordinates - 1
     if INFORMATIVE_TOKENS:
         # Encoder: coordinates of the streamline, except last one
         encoder_tokens = ['p{} -- '.format(i) +
-                          np.array2string(np.asarray(streamline1[i, :]),
+                          np.array2string(np.asarray(streamline[i, :]),
                                           precision=1, separator=', ') +
                           ' -- p{}'.format(i)
                           for i in range(this_seq_len)]
 
         # Decoder input: directions, except the last one, plus SOS.
-        dirs = np.diff(streamline1, n=1, axis=0)
+        dirs = np.diff(streamline, n=1, axis=0)
         decoder_tokens = ['SOS'] + \
                          ['p{} -- '.format(i) +
                           np.array2string(np.asarray(dirs[i]),
@@ -207,52 +178,7 @@ def tto_visualize_weights(args, parser):
         encoder_tokens = ['point {}'.format(i) for i in range(this_seq_len)]
         decoder_tokens = ['SOS'] + \
                          ['dir {}'.format(i) for i in range(this_seq_len - 1)]
-
-    if args.average_heads:
-        # Taking one streamline, one head = twice squeezed. Adding back the
-        # two missing dimensions.
-        encoder_attention = [
-            layer[0, 0:this_seq_len, 0:this_seq_len][None, None, :, :]
-            for layer in sa_encoder]
-        decoder_attention = [
-            layer[0, 0:this_seq_len, 0:this_seq_len][None, None, :, :]
-            for layer in sa_decoder]
-        cross_attention = [
-            layer[0, 0:this_seq_len, 0:this_seq_len][None, None, :, :]
-            for layer in mha_cross]
-
-    else:
-        # Taking one streamline = once squeezed. Adding back the missing
-        # dimension.
-        encoder_attention = [
-            layer[0, :, 0:this_seq_len, 0:this_seq_len][None, :, :]
-            for layer in sa_encoder]
-        decoder_attention = [
-            layer[0, :, 0:this_seq_len, 0:this_seq_len][None, :, :]
-            for layer in sa_decoder]
-        cross_attention = [
-            layer[0, :, 0:this_seq_len, 0:this_seq_len][None, :, :]
-            for layer in mha_cross]
-
-    if NORMALIZE_WEIGHTS:
-        print("Normalizing weights for each time point for a better "
-              "rendering.")
-        # Trying to perceive better: Weights normalized per point.
-        encoder_attention = [torch.nn.functional.normalize(layer, dim=2)
-                             for layer in encoder_attention]
-        decoder_attention = [torch.nn.functional.normalize(layer, dim=2)
-                             for layer in decoder_attention]
-        cross_attention = [torch.nn.functional.normalize(layer, dim=2)
-                           for layer in cross_attention]
-
-    tto_show_head_view(encoder_attention, decoder_attention,
-                       cross_attention, encoder_tokens, decoder_tokens)
-
-    # Model view not useful if we have only one layer, one head.
-    if nb_layers > 1 or nb_heads > 1:
-        tto_show_model_view(encoder_attention, decoder_attention,
-                            cross_attention, encoder_tokens, decoder_tokens,
-                            nb_heads, nb_layers)
+    return encoder_tokens, decoder_tokens
 
 
 def print_head_view_help():
@@ -289,6 +215,12 @@ def tto_show_head_view(encoder_attention, decoder_attention, cross_attention,
               decoder_tokens=decoder_tokens)
 
 
+def ttst_show_head_view(encoder_attention, tokens):
+    print_head_view_help()
+    head_view(encoder_attention=encoder_attention,
+              encoder_tokens=tokens)
+
+
 def tto_show_model_view(encoder_attention, decoder_attention, cross_attention,
                         encoder_tokens, decoder_tokens, nb_heads, nb_layers):
     if SHOW_MODEL_VIEW:
@@ -307,3 +239,15 @@ def tto_show_model_view(encoder_attention, decoder_attention, cross_attention,
                        cross_attention=tmp_c,
                        encoder_tokens=encoder_tokens,
                        decoder_tokens=decoder_tokens)
+
+
+def ttst_show_model_view(encoder_attention, tokens, nb_heads, nb_layers):
+    if SHOW_MODEL_VIEW:
+        print_model_view_help()
+
+        for head in range(nb_heads):
+            print("HEAD #{}".format(head + 1))
+            tmp_e = [encoder_attention[i][:, head, :, :][:, None, :, :]
+                     for i in range(nb_layers)]
+            model_view(encoder_attention=tmp_e,
+                       encoder_tokens=tokens)
--- scripts_python/tto_visualize_weights.py
+++ scripts_python/tto_visualize_weights.py
@@ -1,43 +1,20 @@
 # -*- coding: utf-8 -*-
-import shutil
 import sys
-import os
-from os.path import join, dirname, isfile, isdir
-
-import subprocess
+from os.path import join, dirname
 
 from scilpy.io.fetcher import get_home
-from scilpy.io.utils import assert_outputs_exist, assert_inputs_exist
+from scilpy.io.utils import assert_inputs_exist
 
-from dwi_ml.models.projects.transformer_visualisation_utils import \
-    build_argparser_transformer_visu, tto_visualize_weights
-
-# 1) Finding the jupyter notebook
-dwi_ml_dir = dirname(dirname(__file__))
-IPYNB_FILENAME = join(
-    dwi_ml_dir, 'dwi_ml/models/projects/tto_visualize_weights.ipynb')
-
-# 2) Jupyter notebook needs to know where to load the config file.
-# Needs to be always at the same place.
-# We choose to save it in our pytest dir.
-home = get_home()
-CONFIG_FILENAME = join(home, 'ipynb_tto_visualize_weights.config')
+from dwi_ml.visu.projects.transformer_visualisation import \
+    visualize_weights_using_jupyter, tto_visualize_weights
+from dwi_ml.visu.projects.transformer_visualisation_utils import \
+    build_argparser_transformer_visu
 
 
 def main(argv):
 
     parser = build_argparser_transformer_visu()
     args = parser.parse_args()
-
-    # Adding complete names with out_dir and prefix to args.
-    if args.out_dir is None:
-        args.out_dir = os.path.join(args.experiment_path, 'visu')
-    args.out_html_file = os.path.join(
-        args.out_dir, args.out_prefix + 'tto_visu.html')
-    args.out_ipynb_file = os.path.join(
-        args.out_dir, args.out_prefix + 'tto_visu.ipynb')
-    args.out_config_file = os.path.join(
-        args.out_dir, args.out_prefix + 'tto_visu.config')
 
     assert_inputs_exist(parser, [args.hdf5_file, args.input_streamlines],
                         args.reference)
@@ -47,53 +24,19 @@ def main(argv):
         # Will probably just print some HTML stuff in the terminal...
         tto_visualize_weights(args, parser)
     else:
-        # 1. Test that we correctly found the ipynb file.
-        if not isfile(IPYNB_FILENAME):
-            raise ValueError(
-                "We could not find the jupyter notebook file. Probably a "
-                "coding error on our side. We expected it to be in {}"
-                .format(IPYNB_FILENAME))
+        # 1) Finding the jupyter notebook
+        dwi_ml_dir = dirname(dirname(__file__))
+        ipynb_filename = join(
+            dwi_ml_dir, 'dwi_ml/visu/projects/tto_visualize_weights.ipynb')
 
-        # 2. Verify that output dir exists but not output files.
-        if not isdir(args.out_dir):
-            os.mkdir(args.out_dir)
-        assert_outputs_exist(parser, args, [args.out_html_file,
-                                            args.out_ipynb_file,
-                                            args.out_config_file])
+        # 2) Jupyter notebook needs to know where to load the config file.
+        # Needs to be always at the same place.
+        # We choose to save it in our pytest dir.
+        home = get_home()
+        config_filename = join(home, 'ipynb_tto_visualize_weights.config')
 
-        # 3. Save the args to this script in a config file, to be read again
-        # by the notebook in a new argparser instance.
-        if isfile(CONFIG_FILENAME):  # In case a previous call failed.
-            os.remove(CONFIG_FILENAME)
-        with open(CONFIG_FILENAME, 'w') as f:
-            f.write(' '.join(argv))
-
-        # 4. Copy files locally
-        shutil.copyfile(IPYNB_FILENAME, args.out_ipynb_file)
-        shutil.copyfile(CONFIG_FILENAME, args.out_config_file)
-
-        # 5. Launching.
-        print("\n\n"
-              "** We will by running the jupyter notebook and save its result "
-              "as a html file: {} **\n\n".format(args.out_html_file))
-        try:
-            command = 'jupyter nbconvert --output-dir={:s} --execute {:s} ' \
-                      '--to html'.format(args.out_html_file, IPYNB_FILENAME)
-            print("Running command:\n\n>>{}\n\n".format(command))
-            subprocess.run(command, shell=True, check=True)
-        except subprocess.CalledProcessError:
-            print("JUPYTER NOTEBOOK EXCEUTION WAS NOT SUCCESSFULL.")
-            print("You may try to use --run_locally to debug.")
-            print("You may try to run yourself the notebook copied in "
-                  "--out_dir using the config file.")
-            print("You may try to specify which kernel (i.e. which python "
-                  "version) \nyour jupyter notebooks should run on:\n"
-                  ">> (workon your favorite python environment)\n"
-                  ">> pip install ipykernel\n"
-                  ">> python -m ipykernel install --user\n")
-
-        # 6. Delete config file.
-        os.remove(CONFIG_FILENAME)
+        visualize_weights_using_jupyter(ipynb_filename, config_filename,
+                                        parser, args, argv)
 
 
 if __name__ == '__main__':

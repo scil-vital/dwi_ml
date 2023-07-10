8b9a369b27ea40d3c80b20534dd2a594e9f1c10f
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-07-10 16:04:12 -0400

    update unit tests

--- dwi_ml/unit_tests/test_models_transformers.py
+++ dwi_ml/unit_tests/test_models_transformers.py
@@ -4,7 +4,7 @@ import logging
 from torch import isnan, set_printoptions
 
 from dwi_ml.models.projects.transforming_tractography import (
-    OriginalTransformerModel, TransformerSrcAndTgtModel)
+    OriginalTransformerModel, TransformerSrcAndTgtModel, TransformerSrcOnlyModel)
 from dwi_ml.unit_tests.utils.data_and_models_for_tests import create_test_batch
 
 (batch_x_training, batch_x_tracking,
@@ -30,14 +30,34 @@ def _prepare_original_model():
 
 def _prepare_ttst_model():
     model = TransformerSrcAndTgtModel(
+        # Varying sos_token_type.
+        # ffnn_hidden_size,
+        # norm_first
+        # dg_key
+        # eos
+        # neighborhood
+        # start from copy prev
         experiment_name='test',  step_size=0.5, compress_lines=None,
-        nb_features=4, max_len=5, embedding_size_x=4, embedding_size_t=1,
+        nb_features=4, max_len=5, embedding_size_x=4, embedding_size_t=2,
         log_level='DEBUG', sos_token_type='repulsion100',
         positional_encoding_key='sinusoidal', embedding_key_x='nn_embedding',
-        embedding_key_t='nn_embedding', ffnn_hidden_size=None, nheads=1,
-        dropout_rate=0., activation='relu', norm_first=False, n_layers_e=1,
-        dg_key='cosine-regression', dg_args=None, neighborhood_type=None,
-        neighborhood_radius=None, start_from_copy_prev=False)
+        embedding_key_t='nn_embedding', ffnn_hidden_size=6, nheads=1,
+        dropout_rate=0., activation='relu', norm_first=True, n_layers_e=1,
+        dg_key='sphere-classification', dg_args={'add_eos': True},
+        neighborhood_type='axes', neighborhood_radius=[1, 2],
+        start_from_copy_prev=True)
+    return model
+
+
+def _prepare_tts_model():
+    model = TransformerSrcOnlyModel(
+        experiment_name='test',  step_size=0.5, compress_lines=None,
+        nb_features=4, max_len=5, log_level='DEBUG',
+        positional_encoding_key='sinusoidal', embedding_key_x='nn_embedding',
+        ffnn_hidden_size=None, nheads=1, dropout_rate=0., activation='relu',
+        norm_first=False, n_layers_e=1, dg_key='cosine-regression',
+        dg_args=None, neighborhood_type=None, neighborhood_radius=None,
+        d_model=4)
     return model
 
 
@@ -90,6 +110,29 @@ def _run_ttst_model(model):
     assert not isnan(output[0, 0])
 
 
+def _run_tts_model(model):
+    # Testing forward.
+    logging.debug("\n****** Training")
+    model.set_context('training')
+    output, weights = model(batch_x_training, None, return_weights=True)
+    assert len(output) == nb_streamlines
+    assert output[0].shape[1] == 3  # Here, regression, should output x, y, z
+    assert len(weights) == 1  # Should get weights for encoder self-attention
+    assert not isnan(output[0][0, 0])
+    for weight in weights:
+        assert weight is not None
+
+    # Testing tracking
+    logging.debug("\n****** Tracking")
+    model.set_context('tracking')
+    output = model(batch_x_tracking, batch_s_tracking)
+
+    # Output is not split.
+    assert output.shape[0] == nb_streamlines
+    assert output.shape[1] == 3  # Here, regression, should output x, y, z
+    assert not isnan(output[0, 0])
+
+
 def test_models():
     logging.debug("\n\nOriginal model!\n"
                   "-----------------------------")
@@ -98,6 +141,13 @@ def test_models():
     _run_original_model(model)
 
     # Note. output[0].shape[0] ==> Depends if we unpad sequences.
+
+    logging.debug("\n\nSource only model!\n"
+                  "-----------------------------")
+
+    model = _prepare_tts_model()
+    _run_tts_model(model)
+
 
     logging.debug("\n\nSource and target model!\n"
                   "-----------------------------")

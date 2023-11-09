182f08c26e17f7d5f3132db150844312ecd832d8
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-11-09 17:22:57 -0500

    Fixes. Resampling attention modified.

--- dwi_ml/testing/projects/transformer_visualisation.py
+++ dwi_ml/testing/projects/transformer_visualisation.py
@@ -3,27 +3,29 @@ import argparse
 import logging
 import os
 
-import torch
-
 from scilpy.io.fetcher import get_home as get_scilpy_folder
 from scilpy.io.utils import add_reference_arg, add_bbox_arg, add_overwrite_arg, assert_inputs_exist
 
-from dwi_ml.io_utils import verify_which_model_in_path, add_logging_arg
-from dwi_ml.models.projects.transformer_models import \
-    OriginalTransformerModel, TransformerSrcAndTgtModel
+from dwi_ml.io_utils import verify_which_model_in_path, add_logging_arg, \
+    add_resample_or_compress_arg
+from dwi_ml.models.projects.transformer_models import (
+    OriginalTransformerModel, TransformerSrcOnlyModel,
+    TransformerSrcAndTgtModel)
 
-# Currently, with our quite long sequences compared to their example, this
-# is a bit ugly.
-from dwi_ml.testing.projects.transformer_visualisation_submethods import \
-    load_data_run_model, tto_show_head_view, tto_show_model_view, \
-    tto_prepare_tokens, ttst_prepare_tokens, ttst_show_model_view, \
-    ttst_show_head_view
+from dwi_ml.testing.projects.transformer_visualisation_submethods import (
+    load_data_run_model, prepare_encoder_tokens, prepare_decoder_tokens,
+    reformat_attention, tto_show_head_view, tto_show_model_view,
+    ttst_show_model_view, ttst_show_head_view)
 
 NORMALIZE_WEIGHTS = True
 INFORMATIVE_TOKENS = False
 
 
 def get_config_filename():
+    """
+    File that will be saved by the python script with all the args. The
+    jupyter notebook can then load them again.
+    """
     # We choose to add it in the hidden .scilpy folder in our home.
     # (Where our test data also is).
     hidden_folder = get_scilpy_folder()
@@ -61,17 +63,15 @@ def build_argparser_transformer_visu():
                    help="Subject id should probably come come the "
                         "'testing' set but you can\n modify this to "
                         "'training' or 'validation'.")
-    sub = p.add_mutually_exclusive_group()
-    sub.add_argument(
-        '--step_size', type=float, metavar='s',
-        help="Resample all streamlines to this step size (in mm). "
-             "Default = None.")
-    sub.add_argument(
-        '--compress', action='store_true',
-        help="If set, compress streamlines. Default = Not set.")
+    p.add_argument('--resample_attention', type=int, default=50,
+                   help="Streamline will be sampled as decided by the model."
+                        "However, \nattention will be resampled to fit better "
+                        "in the html page. \n (Resampling is done by "
+                        "averaging the attention every N points).\n"
+                        "Default: 50")
     p.add_argument('--average_heads', action='store_true',
-                   help="If set, average heads when visualising outputs")
-
+                   help="If true, resample all heads (per layer per "
+                        "attention type).")
     p.add_argument(
         '--out_dir', metavar='d',
         help="Output directory where to save the html file, as well as a \n"
@@ -87,6 +87,7 @@ def build_argparser_transformer_visu():
              "to debug. \nWithout this argument, we will run through jupyter "
              "to allow visualizing the results with Bert.")
 
+    add_resample_or_compress_arg(p)
     add_reference_arg(p)
     add_bbox_arg(p)
     add_logging_arg(p)
@@ -96,6 +97,9 @@ def build_argparser_transformer_visu():
 
 
 def tt_visualize_weights(args, parser):
+    """
+    Main part of the script: verifies with type of Transformer we have.
+    """
     # Doing again the verification; in case this is run from the jupyter
     # notebook from an old config file.
     assert_inputs_exist(parser, [args.hdf5_file, args.input_streamlines],
@@ -103,170 +107,85 @@ def tt_visualize_weights(args, parser):
     if not os.path.isdir(args.experiment_path):
         parser.error("Experiment {} not found.".format(args.experiment_path))
 
-    logging.info("Verifying model type.")
+    sub_logger_level = 'WARNING'
+    logging.getLogger().setLevel(level=args.logging)
+
+    logging.info("1) Verifying model type.")
     model_dir = os.path.join(args.experiment_path, 'best_model')
     model_type = verify_which_model_in_path(model_dir)
-    print("Model's class: {}".format(model_type))
+    logging.info("   Model's class: {}\n".format(model_type))
     if model_type == 'OriginalTransformerModel':
-        visualize_weights = tto_visualize_weights
-        jupyter = 'tto_visualize_weights.ipynb'
+        model_cls = OriginalTransformerModel
+        visu_fct = tto_visualize_weights
     elif model_type == 'TransformerSrcAndTgtModel':
-        visualize_weights = ttst_visualize_weights
-        jupyter = 'ttst_visualize_weights.ipynb'
+        model_cls = TransformerSrcAndTgtModel
+        visu_fct = encoder_only_visualize_weights
     elif model_type == 'TransformerSrcOnlyModel':
-        raise NotImplementedError("Not ready yet for this model.")
+        model_cls = TransformerSrcOnlyModel
+        visu_fct = encoder_only_visualize_weights
     else:
         raise ValueError("Model type not a recognized transformer Transformer"
                          "({})".format(model_type))
 
-
-def tto_visualize_weights(args, parser):
-    sub_logger_level = args.logging.upper()
-    if sub_logger_level == 'DEBUG':
-        # make them info max
-        sub_logger_level = 'INFO'
-    logging.getLogger().setLevel(level=args.logging)
-
-    logging.info("Loading model.")
-    model = OriginalTransformerModel.load_model_from_params_and_state(
+    logging.info("2) Loading your model.\n")
+    model = model_cls.load_model_from_params_and_state(
         args.experiment_path + '/best_model', log_level=sub_logger_level)
     model.set_context('visu')
 
-    weights, batch_streamlines = load_data_run_model(parser, args, model)
-    sa_encoder, sa_decoder, mha_cross = weights
+    logging.info("3) Running the model on the bundle...")
+    logging.info("   IMPORTANT !!! Right now the code is only ready to show "
+                 "attention on one streamline at the time!")
+    weights, batch_streamlines = load_data_run_model(
+        parser, args, model, pick_one=True, sub_logger_level=sub_logger_level)
+    logging.info("...Done!\n")
 
-    # ----------------
-    # Shape of attentions are a List of nb_layers x:
-    #                [nb_streamlines, (nb_heads), batch_max_len, batch_max_len]
-    # Reshaping all attentions to List of nb layers x
-    #                [nb_streamlines=1, nb_heads, this_s_len,   this_s_len]
-    # (nb heads dimension is skipped if args.average_heads).
-    # -----------------
-    nb_layers = len(sa_encoder)
-    nb_heads = 1 if args.average_heads else model.nheads
-
+    logging.info("4) Preparing visu...")
     streamline1 = batch_streamlines[0]
+    visu_fct(weights, streamline1, args.resample_attention,
+             model.direction_getter.add_eos,)
+    logging.info("... Done!")
+
+
+def tto_visualize_weights(weights, streamline1, resample_attention: int,
+                          add_eos: bool):
+
+    # Original model: 3 attentions.
+    encoder_attention, decoder_attention, cross_attention = weights
+
+    # Unpadding
     this_seq_len = len(streamline1)
-    if model.direction_getter.add_eos:
-        logging.info("Chosen streamline's length: {} points"
-                     .format(len(streamline1)))
-    else:
-        logging.info("Chosen streamline's length (after removing the last "
-                     "unused point): {} points".format(len(streamline1)))
 
-    encoder_tokens, decoder_tokens = tto_prepare_tokens(streamline1)
+    logging.info("   Preparing encoder attention")
+    encoder_attention, ind = reformat_attention(
+        encoder_attention, this_seq_len, NORMALIZE_WEIGHTS, resample_attention)
+    logging.info("   Preparing decoder attention")
+    decoder_attention, _ = reformat_attention(
+        decoder_attention, this_seq_len, NORMALIZE_WEIGHTS, resample_attention)
+    logging.info("   Preparing cross-attention attention")
+    cross_attention, _ = reformat_attention(
+        cross_attention, this_seq_len, NORMALIZE_WEIGHTS, resample_attention)
 
-    # Unpadding attention
-    if args.average_heads:
-        print("Averaging heads!")
-        # Taking one streamline, one head = twice squeezed. Adding back the
-        # two missing dimensions.
-        encoder_attention = [
-            layer[0, 0:this_seq_len, 0:this_seq_len][None, None, :, :]
-            for layer in sa_encoder]
-        decoder_attention = [
-            layer[0, 0:this_seq_len, 0:this_seq_len][None, None, :, :]
-            for layer in sa_decoder]
-        cross_attention = [
-            layer[0, 0:this_seq_len, 0:this_seq_len][None, None, :, :]
-            for layer in mha_cross]
-
-    else:
-        print("Number of heads: Encoder: {}. Decoder: {}. Mixed: {}"
-              .format(sa_encoder[0].shape[1], sa_decoder[0].shape[1],
-                      mha_cross[0].shape[1]))
-        # Taking one streamline = once squeezed. Adding back the missing
-        # dimension.
-        encoder_attention = [
-            layer[0, :, 0:this_seq_len, 0:this_seq_len][None, :, :]
-            for layer in sa_encoder]
-        decoder_attention = [
-            layer[0, :, 0:this_seq_len, 0:this_seq_len][None, :, :]
-            for layer in sa_decoder]
-        cross_attention = [
-            layer[0, :, 0:this_seq_len, 0:this_seq_len][None, :, :]
-            for layer in mha_cross]
-
-    if NORMALIZE_WEIGHTS:
-        print("Normalizing weights for each time point for a better "
-              "rendering.")
-        # Trying to perceive better: Weights normalized per point.
-        encoder_attention = [torch.nn.functional.normalize(layer, dim=2)
-                             for layer in encoder_attention]
-        decoder_attention = [torch.nn.functional.normalize(layer, dim=2)
-                             for layer in decoder_attention]
-        cross_attention = [torch.nn.functional.normalize(layer, dim=2)
-                           for layer in cross_attention]
+    encoder_tokens = prepare_encoder_tokens(streamline1, add_eos, ind)
+    decoder_tokens = prepare_decoder_tokens(streamline1, ind)
 
     tto_show_head_view(encoder_attention, decoder_attention,
                        cross_attention, encoder_tokens, decoder_tokens)
-
-    # Model view not useful if we have only one layer, one head.
-    if nb_layers > 1 or nb_heads > 1:
-        tto_show_model_view(encoder_attention, decoder_attention,
-                            cross_attention, encoder_tokens, decoder_tokens,
-                            nb_heads, nb_layers)
+    tto_show_model_view(encoder_attention, decoder_attention,
+                        cross_attention, encoder_tokens, decoder_tokens)
 
 
-def ttst_visualize_weights(args, parser):
-    sub_logger_level = args.logging.upper()
-    if sub_logger_level == 'DEBUG':
-        # make them info max
-        sub_logger_level = 'INFO'
-    logging.getLogger().setLevel(level=args.logging)
+def encoder_only_visualize_weights(
+        weights, streamline1, resample_attention: int, add_eos: bool):
+    encoder_attention, = weights
 
-    logging.info("Loading model.")
-    model = TransformerSrcAndTgtModel.load_model_from_params_and_state(
-        args.experiment_path + '/best_model', log_level=sub_logger_level)
-    model.set_context('visu')
-
-    weights, batch_streamlines = load_data_run_model(parser, args, model)
-    sa_encoder, = weights
-
-    # ----------------
-    # Shape of attentions are a List of nb_layers x:
-    #                [nb_streamlines, (nb_heads), batch_max_len, batch_max_len]
-    # Reshaping all attentions to List of nb layers x
-    #                [nb_streamlines=1, nb_heads, this_s_len,   this_s_len]
-    # (nb heads dimension is skipped if args.average_heads).
-    # -----------------
-    nb_layers = len(sa_encoder)
-    nb_heads = 1 if args.average_heads else model.nheads
-
-    streamline1 = batch_streamlines[0]
     this_seq_len = len(streamline1)
-    if model.direction_getter.add_eos:
-        logging.info("Chosen streamline's length: {} points"
-                     .format(len(streamline1)))
-    else:
-        logging.info("Chosen streamline's length (after removing the last "
-                     "unused point): {} points".format(len(streamline1)))
+    logging.info("   Preparing encoder attention (the only attention in this "
+                 "model)")
 
-    tokens = ttst_prepare_tokens(streamline1)
+    encoder_attention, ind = reformat_attention(
+        encoder_attention, this_seq_len, NORMALIZE_WEIGHTS, resample_attention)
 
-    # Unpadding attention
-    if args.average_heads:
-        # Taking one streamline, one head = twice squeezed. Adding back the
-        # two missing dimensions.
-        encoder_attention = [
-            layer[0, 0:this_seq_len, 0:this_seq_len][None, None, :, :]
-            for layer in sa_encoder]
-    else:
-        # Taking one streamline = once squeezed. Adding back the missing
-        # dimension.
-        encoder_attention = [
-            layer[0, :, 0:this_seq_len, 0:this_seq_len][None, :, :]
-            for layer in sa_encoder]
+    encoder_tokens = prepare_encoder_tokens(streamline1, add_eos, ind)
 
-    if NORMALIZE_WEIGHTS:
-        print("Normalizing weights for each time point for a better "
-              "rendering.")
-        # Trying to perceive better: Weights normalized per point.
-        encoder_attention = [torch.nn.functional.normalize(layer, dim=2)
-                             for layer in encoder_attention]
-
-    ttst_show_head_view(encoder_attention, tokens)
-
-    # Model view not useful if we have only one layer, one head.
-    if nb_layers > 1 or nb_heads > 1:
-        ttst_show_model_view(encoder_attention, tokens, nb_heads, nb_layers)
+    ttst_show_head_view(encoder_attention, encoder_tokens)
+    ttst_show_model_view(encoder_attention, encoder_tokens)

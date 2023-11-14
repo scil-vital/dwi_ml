1a30f178b74a0563f48fa9ac079dbd731fe448f1
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2023-11-14 11:26:53 -0500

    Add visu as imshow plot. Better than their model_view

--- dwi_ml/testing/projects/transformer_visualisation_submethods.py
+++ dwi_ml/testing/projects/transformer_visualisation_submethods.py
@@ -7,6 +7,8 @@ import torch
 
 # Ref: # https://github.com/jessevig/bertviz
 from bertviz import model_view, head_view
+from matplotlib import pyplot as plt
+from skimage.measure import block_reduce
 
 from scilpy.io.streamlines import load_tractogram_with_reference
 from scilpy.tractograms.streamline_operations import \
@@ -53,6 +55,7 @@ def load_data_run_model(parser, args, model: AbstractTransformerModel,
     else:
         streamline_ids = 0
 
+    streamline_ids = 3544 #    ---------------------------------------------------------------- A EFFACER
     # Bug in dipy. If accessing only one streamline, then sft.streamlines is
     # not a list of streamlines anymore, but the streamline itself. Thus,
     # len(sft) = nb_points instead of 1.
@@ -106,53 +109,72 @@ def load_data_run_model(parser, args, model: AbstractTransformerModel,
     return weights, streamlines
 
 
-def reformat_attention(attention, this_seq_len, normalize_weights: bool,
-                       resample_attention: int):
-    # ----------------
-    # Shape of attentions are a List of nb_layers x:
-    #   [nb_streamlines, nb_heads, batch_max_len, batch_max_len]
+def reformat_attention(attention, this_seq_len, resample_attention: int):
 
-    # Reshaping all attentions to List of nb layers x
-    #   [nb_streamlines=1, nb_heads, this_s_len,   this_s_len]
-
-    # (nb heads dimension is skipped if average_heads).
-    # -----------------
-    for l in range(len(attention)):   # Per layer
+    for ll in range(len(attention)):   # Per layer
+        # Easier to work with numpy. Will put back to tensor after.
+        att = attention[ll].numpy()
 
         # Unpadding. Taking one streamline. Adding back the dimension missing.
-        if len(attention[l].shape) == 3:
-            # Heads have already been averaged. Adding one more dimension.
-            attention[l] = attention[l][0, 0:this_seq_len, 0:this_seq_len]
-            attention[l] = attention[l][None, None, :, :]
+        if len(att.shape) == 3:
+            # Shape of attentions are a List of nb_layers x:
+            #   [nb_streamlines, batch_max_len, batch_max_len]
+            att = att[0, 0:this_seq_len, 0:this_seq_len]
+            att = att[None, None, :, :]
         else:
-            attention[l] = attention[l][0, :, 0:this_seq_len, 0:this_seq_len]
-            attention[l] = attention[l][None, :, :]
+            # Shape of attentions are a List of nb_layers x:
+            #   [nb_streamlines, nb_heads, batch_max_len, batch_max_len]
+            att = att[0, :, 0:this_seq_len, 0:this_seq_len]
+            att = att[None, :, :]
 
-        if normalize_weights:
-            # Trying to perceive better: Weights normalized per point.
-            attention[l] = torch.nn.functional.normalize(attention[l], dim=2)
+        # Right now shape of attention is:
+        #     [nb_streamlines=1, nb_heads, this_s_len, this_s_len]
+        # (nb_heads = 1 if average_heads).
+        
+        # Normalizing weight. Without it, we rapidly see nothing!
+        # Easier to see when we normalize on the x axis.
+
+        # Version 1) L_p pnormalization ==> V = V / norm(v)
+        # attention[ll] = torch.nn.functional.normalize(attention[ll], dim=2)
+        # attention[ll] = torch.nn.functional.normalize(attention[ll], dim=3)
+
+        # Version 2) To percentages
+        # Values seem already normalized in the last axis, so this is ~
+        # equivalent to doing * 100. Bertviz seem to have a fixed colorscale
+        # because this shows better.
+
+        # Version 3)
+        # Normalizing each row so that max value = 1.
+        print("Normalizing attention at each point to range [0, 1]")
+        max_ = np.max(att, axis=3)
+        att = att / max_[:, :, :, None]
+
 
         if resample_attention < this_seq_len:
+            print("RESAMPLING ATTENTION TO A SEQUENCE OF LENGTH {}\n"
+                  "(uses the max per block)"
+                  .format(resample_attention))
+
             nb_together = this_seq_len / resample_attention
-            ind = [(int(i*nb_together),
-                    min(this_seq_len - 1, int((i + 1) * nb_together)))
-                   for i in range(resample_attention)]
+            nb_together = int(np.floor(nb_together))
 
-            # Averaging left side of attention visu
-            # Could use int(np.floor), but just int does the same.
-            averaged = [torch.mean(
-                attention[l][:, :, ind[i][0]: ind[i][1], :], dim=2)
-                for i in range(resample_attention)]
-            attention[l] = torch.stack(averaged, dim=2)
+            ind1 = np.arange(0, this_seq_len, nb_together)
+            ind = [(i, min(i + nb_together, this_seq_len - 1))
+                   for i in ind1]
 
-            # Averaging right side of attention visu
-            averaged = [torch.mean(
-                attention[l][:, :, :, ind[i][0]: ind[i][1]], dim=3)
-                for i in range(resample_attention)]
-            attention[l] = torch.stack(averaged, dim=3)
+            # No option to pad to edge value in block_reduce. Doing manually.
+            missing = (len(ind) * nb_together) - att.shape[2]
+            att = np.pad(att, ((0, 0), (0, 0), (0, missing), (0, missing)),
+                         mode='edge')
+
+            att = block_reduce(
+                att, block_size=(1, 1, nb_together, nb_together),
+                func=np.max, cval=1000.0)  # 1000: to see if bug.
 
         else:
             ind = None
+
+        attention[ll] = torch.as_tensor(att)
 
     return attention, ind
 
@@ -237,6 +259,48 @@ def tto_show_head_view(encoder_attention, decoder_attention, cross_attention,
               encoder_tokens=encoder_tokens, decoder_tokens=decoder_tokens)
 
 
+def show_model_view_as_imshow(attention, tokens_x, tokens_y=None):
+    torch.set_printoptions(precision=2, sci_mode=False, linewidth=150)
+
+    nb_layers = len(attention)
+    size_x = len(tokens_x)
+
+    if tokens_y is None:
+        tokens_y = tokens_x.copy()
+    size_y = len(tokens_y)
+
+    cmap = plt.get_cmap("jet")
+    cmap.set_bad(color='black')
+
+    for i in range(nb_layers):
+        att = attention[i].numpy()
+        nb_heads = att.shape[1]
+        fig, axs = plt.subplots(1, nb_heads, figsize=(20, 8))
+
+        for h in range(nb_heads):
+            a = np.squeeze(att[0, h, :, :])
+            a = np.ma.masked_where(a == 0, a)
+            im = axs[h].imshow(a, interpolation='None')
+            axs[h].set_title("Head {}".format(h))
+            axs[h].set_xticks(np.arange(size_x), fontsize=10)
+            axs[h].set_yticks(np.arange(size_y), fontsize=10)
+            axs[h].set_xticklabels(tokens_x, rotation=-90)
+            axs[h].set_yticklabels(tokens_y)
+
+            plt.colorbar(im, ax=axs[h])
+
+        plt.suptitle("Layer {}\n"
+                     "Each row: When getting next direction for this point, "
+                     "what do we look at?\n"
+                     "Each column: This data was used to decide which "
+                     "directions?\n"
+                     "DATA IS NORMALIZED TO [0-1] RANGE PER ROW".format(i))
+
+        fig.tight_layout()
+
+    plt.show()
+
+
 def ttst_show_head_view(encoder_attention, tokens):
     print_head_view_help()
     head_view(encoder_attention=encoder_attention, encoder_tokens=tokens)
@@ -249,7 +313,7 @@ def tto_show_model_view(encoder_attention, decoder_attention, cross_attention,
     nb_layers = len(encoder_attention)
 
     # Model view not useful if we have only one layer, one head.
-    if SHOW_MODEL_VIEW and (nb_layers > 1 or nb_heads > 1):
+    if SHOW_MODEL_VIEW:
         print_model_view_help()
 
         for head in range(nb_heads):
@@ -263,6 +327,16 @@ def tto_show_model_view(encoder_attention, decoder_attention, cross_attention,
             model_view(encoder_attention=tmp_e, decoder_attention=tmp_d,
                        cross_attention=tmp_c, encoder_tokens=encoder_tokens,
                        decoder_tokens=decoder_tokens)
+    else:
+        print("ENCODER ATTENTION: ")
+        show_model_view_as_imshow(encoder_attention,
+                                  encoder_tokens, encoder_tokens)
+        print("DECODER ATTENTION: ")
+        show_model_view_as_imshow(decoder_attention,
+                                  decoder_tokens, decoder_tokens)
+        print("CROSS ATTENTION: ")
+        show_model_view_as_imshow(cross_attention,
+                                  encoder_tokens, decoder_tokens)
 
 
 def ttst_show_model_view(encoder_attention, tokens):
@@ -278,3 +352,7 @@ def ttst_show_model_view(encoder_attention, tokens):
             tmp_e = [encoder_attention[i][:, head, :, :][:, None, :, :]
                      for i in range(nb_layers)]
             model_view(encoder_attention=tmp_e, encoder_tokens=tokens)
+
+    else:
+        print("ENCODER ATTENTION: ")
+        show_model_view_as_imshow(encoder_attention, tokens)

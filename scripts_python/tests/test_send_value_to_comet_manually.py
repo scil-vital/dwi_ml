2941c2108447a0248c07ba1acdce617feb57ad86
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2024-03-04 10:33:23 -0500

    Add test send_value_to_comet

--- scripts_python/tests/test_send_value_to_comet_manually.py
+++ scripts_python/tests/test_send_value_to_comet_manually.py
@@ -2,11 +2,10 @@
 # -*- coding: utf-8 -*-
 
 def test_help_option(script_runner):
-    ret = script_runner.run('dwiml_compute_connectivity_matrix_from_labels.py',
-                            '--help')
+    ret = script_runner.run('dwiml_send_value_to_comet_manually.py', '--help')
     assert ret.success
 
 
 def test_execution(script_runner):
-    # Impossible for now, no labels file. Could use data from scilpy's tests.
+    # Impossible
     pass
